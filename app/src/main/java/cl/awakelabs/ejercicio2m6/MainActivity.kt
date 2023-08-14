a353e0a1ec64a073ebacc3aa769c013fea7b562f
Alex Muñoz <lomejor77@gmail.com>
2023-08-14 10:34:28 -0400

    Ejercicio 2 modulo 6 implementacion Room. iniciado con dependencias

---
+++ app/src/main/java/cl/awakelabs/ejercicio2m6/MainActivity.kt
@@ -0,0 +1,12 @@
+package cl.awakelabs.ejercicio2m6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cl.awakelabs.ejercicio2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
