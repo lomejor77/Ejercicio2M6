fbc21cdb23510ed76442a18af5d2d4740dc0e5ac
Alex Muñoz <lomejor77@gmail.com>
2023-08-14 11:09:31 -0400

    Ejercicio 2 modulo 6 Hito 1: incorporado repositorio

---
+++ app/src/main/java/cl/awakelabs/ejercicio2m6/Repository.kt
@@ -0,0 +1,15 @@
+package cl.awakelabs.ejercicio2m6
+
+import androidx.lifecycle.LiveData
+import cl.awakelabs.ejercicio2m6.data.local.Item
+import cl.awakelabs.ejercicio2m6.data.local.ItemDAO
+
+class Repository(private val itemDAO: ItemDAO) {
+    suspend fun insertItem(item: Item) {
+        itemDAO.insertItem(item)
+    }
+
+    fun getItem(): LiveData<List<Item>>{
+        return itemDAO.getAllItems()
+    }
+}
